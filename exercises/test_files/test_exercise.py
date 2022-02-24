d4c058bac0427c614f8d411ae1fba61ffe71fe96
LH <hildebrand@fh-aachen.de>
2022-02-24 13:43:04 +0100

    first rough draft for tests detecting ifs possible WIP: much

---
+++ exercises/test_files/test_exercise.py
@@ -0,0 +1,11 @@
+if 2 > 1:
+    print("Ditte is en Test")
+    print("zweite Zeile")
+
+
+def stupid_function(a, b):
+    """calculate a**b"""
+    result = b**a
+
+    if 2 < 4:
+        return result
