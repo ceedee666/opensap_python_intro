30ed1d3fe3896e4c279016d54d1326f6d41d27fc
Christian Drumm <christian.drumm@hey.com>
2022-03-07 10:50:27 +0100

    moved 1.4 to 1.5

--- exercises/week_1/1.5/exercise.py
+++ exercises/week_1/1.5/exercise.py
@@ -1,8 +1,12 @@
-name = input("Your Name: ")
+number_1 = int(input("Please enter the first number: "))
+number_2 = int(input("Please enter the second number: "))
+number_3 = int(input("Please enter the third number: "))
 
-if(name == "Harry Potter"):
-    print("Welcome to Gryffindor, Mr. Potter!")
-else:
-    "Sorry, Hogwarts is full."
+largest_number = number_1
 
+if number_2 > largest_number:
+    largest_number = number_2
+if number_3 > largest_number:
+    largest_number = number_3
 
+print("The largest number is", largest_number)
