fb847da5cbe25082035a7cc82eb198d845674e36
Christian Drumm <christian.drumm@hey.com>
2022-03-07 10:48:02 +0100

    fiexed a typo

--- exercises/week_1/1.3/exercise.py
+++ exercises/week_1/1.3/exercise.py
@@ -1,6 +1,6 @@
 name = input("Please enter a name: ")
 start = input("Please enter a start city: ")
 dest = input("Please enter a destination city: ")
-transport = input("Please enter a means of transprotation: ")
+transport = input("Please enter a means of transportation: ")
 
 print(name, "wants to travel from", start, "to", dest, "by", transport)
