774fd7dc6a9f00d42971a725c23a6927395d57c7
LH <hildebrand@fh-aachen.de>
2022-03-07 17:09:44 +0100

    added example to check if in function body

--- exercises/test_files/sample_exercise.py
+++ exercises/test_files/sample_exercise.py
@@ -1,8 +1,10 @@
 if 2 > 1:
     print("Ditte is en Test")
 
+if True:
+    pass
 
-def template_function(a, b):
+def stupid_function(a, b):
     """calculate a**b"""
 
     result = a**b
--- exercises/test_files/sample_test.py
+++ exercises/test_files/sample_test.py
@@ -52,17 +52,28 @@ class Analyzer(ast.NodeVisitor):
     """Sample Analyzer class to parse & process AST"""
 
     def __init__(self):
-        self.stats = []                     # create empty stats list
+        self.stats_if = []                     # create empty stats list
+        self.stats_fct_if = []                 # create empty stats list for if inside of function
 
 
-    def visit_If(self, node):               # list number of ifs in test file
-        self.stats.append(node.lineno)      # append line occurrences of ifs
+    def visit_If(self, node):                   # list number of ifs in test file
+        self.stats_if.append(node.lineno)       # append line occurrences of ifs
+        self.num_ifs = len(self.stats_if)       # return no of ifs
         self.generic_visit(node)
-        self.num_ifs = len(self.stats)      # return no of ifs
+
+
+    def visit_FunctionDef(self, node):
+        """check whether an if is contained in a function body"""
+        for ast_element in node.body:                           # node.body contains everything inside the function
+            if type(ast_element) == ast.If:                     # check type for every element inside the function node
+                self.stats_fct_if.append(ast_element.lineno)    # append line no of if to list
+        self.num_fct_ifs = len(self.stats_fct_if)               # return no of ifs in functions
+        self.generic_visit(node)
 
 
     def report(self):
-        print(self.stats)
+        print(self.stats_if)
+        print(self.stats_fct_if)
 
 
 
@@ -78,16 +89,28 @@ class Testing(unittest.TestCase):
         self.code, self.std_out, self.error_out, _ = runcaptured("sample_exercise.py")
 
 
+    def test_if_in_fct(self):
+        tree = ast.parse(self.code)                         # build the AST
+        analyzer = Analyzer()                               # create Analyzer instance
+        analyzer.visit(tree)                                # visit the nodes of the AST
+
+        expected_ifs_in_fct = 1
+        self.assertEqual(expected_ifs_in_fct, analyzer.num_fct_ifs,
+            f"You did not use the demanded number of ifs (3), you used if clauses in lines: {analyzer.stats_fct_if}")
+
+
+
+
     def test_ast_parse(self):
         tree = ast.parse(self.code)                         # build the AST
         analyzer = Analyzer()                               # create Analyzer instance
         analyzer.visit(tree)                                # visit the nodes of the AST
-        analyzer.report()                                   # report result(s)
+        analyzer.report()                                   # report result(s), can be removed
 
         # test number of ifs
         expected_ifs = 3
         self.assertEqual(expected_ifs, analyzer.num_ifs,
-            f"You did not use the demanded number of ifs (3), you used if clauses in lines: {analyzer.stats}")
+            f"You did not use the demanded number of ifs (3), you used if clauses in lines: {analyzer.stats_if}")
 
 
     def test_st_fct(self):
