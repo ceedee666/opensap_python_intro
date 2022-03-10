574c3ece4c27163b387d20517e8ca7d1fb0ed43a
Christian Drumm <christian.drumm@hey.com>
2022-03-10 09:32:02 +0100

    Updated exercise for week 2 unit 2

--- exercises/week_2/2.2/exercise.md
+++ exercises/week_2/2.2/exercise.md
@@ -1,14 +1,22 @@
 # Exercise
 
-Create a Python list containing the titles of all [Star Wars](https://en.wikipedia.org/wiki/Star_Wars)
-movies. The should be constucted as follows:
+Below you find a code snippet to reate a Python list containing the titles of all [Star Wars](https://en.wikipedia.org/wiki/Star_Wars)
+movies. The list contains:
 
-- The first element of the list is a list containing the titles of the prequel trilogy:
+- a list containing the titles of the prequel trilogy:
   _The Phantom Menace_, _Attack of the Clones_, _Revenge of the Sith_
-- The second element of the list is a list containing the titles of the original trilogy:
+- a list containing the titles of the original trilogy:
   _A New Hope_, _The Empire Strikes Back_, _Return of the Jedi_,
-- The third element of the list is a list containing the titles of the sequel trilogy:
+- a list containing the titles of the sequel trilogy:
   _The Force Awakens_, _The Last Jedi_, _The Rise of Skywalker_
 
-Assign this list to a variable named `star_wars_movies`.
-Next, write the statement to access the last movie of the original trilogy and print its title.
+```python
+star_wars_movies = [
+    ["The Phantom Menace", "Attack of the Clones", "Revenge of the Sith"],
+    ["A New Hope", "The Empire Strikes Back", "Return of the Jedi"],
+    ["The Force Awakens", "The Last Jedi", "The Rise of Skywalker"],
+]
+```
+
+Write a program that ask the user for a number of the trilogy (1,2 or 3) and the number of the film in
+this trilogy (1, 2 or 3). Print the title of the film corresponding to the user selection.
--- exercises/week_2/2.2/exercise_tests.py
+++ exercises/week_2/2.2/exercise_tests.py
@@ -4,7 +4,7 @@ import io
 import os
 import sys
 import unittest
-from unittest import TestCase
+from unittest import TestCase, mock
 
 sys.modules["assess"] = sys.modules[__name__]
 dirname = os.path.dirname(__file__)
@@ -50,16 +50,14 @@ def runcaptured(tracing=None, variables=None):
 
 class Analyzer(ast.NodeVisitor):
     def __init__(self):
-        self.stats = {"print": 0, "vars": set(), "list_dimensions": []}
+        self.stats = {"print": 0, "input": 0, "vars": set()}
 
     def visit_Call(self, node):
         if node.func.id == "print":
             self.stats["print"] += 1
 
-            if len(node.args) == 1:
-                if isinstance(node.args[0], ast.Subscript):
-                    if node.args[0].value.value.id == "star_wars_movies":
-                        self.stats["correct_var_in_print"] = True
+        if node.func.id == "input":
+            self.stats["input"] += 1
 
         self.generic_visit(node)
 
@@ -67,21 +65,40 @@ class Analyzer(ast.NodeVisitor):
         self.stats["vars"].add(node.targets[0].id)
         self.generic_visit(node)
 
-    def visit_List(self, node):
-        self.stats["list_dimensions"].append(len(node.elts))
-
-        self.generic_visit(node)
-
 
 class Testing(TestCase):
-    def test_output(self):
-        self.code, self.std_out, self.error_out, _ = runcaptured()
-        expected_out = "Return of the Jedi"
+    @mock.patch("builtins.input", create=True)
+    def test_output(self, mocked_input):
+        mocked_input.side_effect = ["1", "1"]
 
-        self.assertEqual(
-            self.std_out.getvalue().strip(),
+        self.code, self.std_out, self.error_out, _ = runcaptured()
+        expected_out = "The Phantom Menace"
+
+        self.assertIn(
             expected_out,
-            "The output of your program is not correct. The expected output is: Return of the Jedi",
+            self.std_out.getvalue().strip(),
+            "The output of your program is not correct. The expected output for the input 1 and 1 is: The Phantom Menace",
+        )
+
+        mocked_input.side_effect = ["3", "3"]
+
+        self.code, self.std_out, self.error_out, _ = runcaptured()
+        expected_out = "The Rise of Skywalker"
+
+        self.assertIn(
+            expected_out,
+            self.std_out.getvalue().strip(),
+            "The output of your program is not correct. The expected output for the input 3 and 3 is: The Rise of Skywalker",
+        )
+        mocked_input.side_effect = ["2", "2"]
+
+        self.code, self.std_out, self.error_out, _ = runcaptured()
+        expected_out = "The Empire Strikes Back"
+
+        self.assertIn(
+            expected_out,
+            self.std_out.getvalue().strip(),
+            "The output of your program is not correct. The expected output for the input 2 and 2 is: The Empire Strikes Back",
         )
 
     def test_source_code(self):
@@ -96,24 +113,14 @@ class Testing(TestCase):
                     "You should define a variable named star_wars_movies.",
                 )
 
-            if analyzer.stats["list_dimensions"][0] != 3:
-                self.fail(
-                    "The variable star_wars_movies should contain a list of length 3.",
-                )
-
-            if not all([x == 3 for x in analyzer.stats["list_dimensions"][1:]]):
-                self.fail(
-                    "All elements of the list star_wars_movies should again be lists of length 3.",
-                )
-
             if analyzer.stats["print"] != 1:
                 self.fail(
                     "You should use the print function one time.",
                 )
 
-            if "correct_var_in_print" not in analyzer.stats:
+            if analyzer.stats["input"] != 2:
                 self.fail(
-                    "You should use the variable star_wars_movies when calling the print function.",
+                    "You should use the input function two times.",
                 )
 
 
