c434e605f5e5e2c8ff6dbbaa2ff0edc57269b8d9
LH <hildebrand@fh-aachen.de>
2022-03-31 17:21:05 +0200

    added functional_tests for ex_4.3

---
+++ exercises/week_4/4.3/functional_tests.py
@@ -0,0 +1,69 @@
+import contextlib, io, ast, unittest
+
+
+@contextlib.contextmanager
+def capture():
+    """Helper function to get std(out&err)"""
+
+    global captured_out
+    import sys
+
+    oldout, olderr = sys.stdout, sys.stderr
+    try:
+        out = [io.StringIO(), io.StringIO()]
+        captured_out = out
+        sys.stdout, sys.stderr = out
+        yield out
+    finally:
+        sys.stdout, sys.stderr = oldout, olderr
+        out[0] = out[0].getvalue()
+        out[1] = out[1].getvalue()
+
+
+@contextlib.contextmanager
+def trace(t):
+    try:
+        if t:
+            t.start()
+        yield
+    finally:
+        if t:
+            t.stop()
+
+
+def runcaptured(filename, tracing=None, variables=None):
+    """Run a specified python file and return source code, stdout, stderr and variables"""
+
+    with open(filename) as f:
+        source = f.read()
+        c = compile(source, filename, "exec")
+        with capture() as out, trace(tracing):
+            if variables is None:
+                variables = {}
+            exec(c, variables)
+        return source, out[0], out[1], variables
+
+
+class Testing(unittest.TestCase):
+    """Testing class with multiple tests"""
+
+    @classmethod
+    def setUpClass(self):
+        """Setup for just-once actions"""
+
+        super().setUpClass()
+        self.code, self.std_out, self.error_out, _ = runcaptured("exercise.py")
+
+    def test_output(self):
+        """Test std_out from user program"""
+
+        expected_out = "9853\n9760\n9745\n"
+        self.assertEqual(
+            expected_out,
+            self.std_out,
+            "Your program did not print the expected result.",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
