a5e111c3e689748eaa9188908678e9b4121e9fd8
LH <hildebrand@fh-aachen.de>
2022-04-01 16:24:46 +0200

    finished functional_tests in ex_4-4

---
+++ exercises/week_4/4.4/functional_tests.py
@@ -0,0 +1,89 @@
+import contextlib, io, ast, unittest
+
+
+@contextlib.contextmanager
+def capture():
+    """Helper function to get std(out&err)"""
+
+    global captured_out
+    import sys
+
+    oldout, olderr = sys.stdout, sys.stderr
+    try:
+        out = [io.StringIO(), io.StringIO()]
+        captured_out = out
+        sys.stdout, sys.stderr = out
+        yield out
+    finally:
+        sys.stdout, sys.stderr = oldout, olderr
+        out[0] = out[0].getvalue()
+        out[1] = out[1].getvalue()
+
+
+@contextlib.contextmanager
+def trace(t):
+    try:
+        if t:
+            t.start()
+        yield
+    finally:
+        if t:
+            t.stop()
+
+
+def runcaptured(filename, tracing=None, variables=None):
+    """Run a specified python file and return source code, stdout, stderr and variables"""
+
+    with open(filename) as f:
+        source = f.read()
+        c = compile(source, filename, "exec")
+        with capture() as out, trace(tracing):
+            if variables is None:
+                variables = {}
+            exec(c, variables)
+        return source, out[0], out[1], variables
+
+
+class Testing(unittest.TestCase):
+    """Testing class with multiple tests"""
+
+    @classmethod
+    def setUpClass(self):
+        """Setup for just-once actions"""
+
+        super().setUpClass()
+        self.code, self.std_out, self.error_out, _ = runcaptured(
+            "reference.py"
+        )  # TODO: change
+
+    def test_output(self):
+        """Test std_out from user program"""
+
+        expected_out = "List of even numbers created!\n"
+        self.assertEqual(
+            expected_out,
+            self.std_out,
+            "Your program did not print the expected result.",
+        )
+
+    def test_output_file(self):
+        """Test if expected output file exists and compare with reference file"""
+
+        with open("even_numbers_ref.txt", "r") as reference:
+            reference_file_out = reference.read()
+
+        try:
+            with open("even_numbers.txt", "r") as input_file:
+                self.assertEqual(
+                    reference_file_out,
+                    input_file.read(),
+                    "The content of your output file is not correct.",
+                )
+        except FileNotFoundError:
+            self.fail(
+                "The expected file 'even_numbers.txt' was not found. Did you create it?"
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
