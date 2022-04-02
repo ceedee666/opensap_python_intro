e9d8cbaa08641c70d7d355738ffef9fda1398769
Christian Drumm <christian.drumm@hey.com>
2022-04-02 22:56:28 +0200

    created tests for week5 exercise 2

---
+++ exercises/week_5/5.2/functional_tests.py
@@ -0,0 +1,72 @@
+import contextlib
+import io
+import os
+import sys
+import unittest
+from unittest import TestCase, mock
+
+sys.modules["assess"] = sys.modules[__name__]
+dirname = os.path.dirname(__file__)
+
+
+@contextlib.contextmanager
+def capture():
+    global captured_out
+    import sys
+
+    oldout, olderr = sys.stdout, sys.stderr
+    try:
+        out = [io.StringIO(), io.StringIO()]
+        captured_out = out
+        sys.stdout, sys.stderr = out
+        yield out
+    finally:
+        sys.stdout, sys.stderr = oldout, olderr
+
+
+@contextlib.contextmanager
+def trace(t):
+    try:
+        if t:
+            t.start()
+        yield
+    finally:
+        if t:
+            t.stop()
+
+
+def runcaptured(tracing=None, variables=None):
+    filename = "exercise.py"
+    with open(filename) as f:
+        source = f.read()
+        c = compile(source, filename, "exec")
+        with capture() as out, trace(tracing):
+            if variables is None:
+                variables = {}
+            exec(c, variables)
+        return source, out[0], out[1], variables
+
+
+class Testing(TestCase):
+    @mock.patch("builtins.input", create=True)
+    def test_output_not_changed(self, mocked_input):
+        name = "Potter"
+        first_name = "Harry"
+        student_id = "666555"
+
+        mocked_input.side_effect = [name, first_name, student_id]
+
+        self.code, self.std_out, self.error_out, _ = runcaptured()
+
+        output = self.std_out.getvalue().strip()
+
+        expected_out = f"('{name}', '{first_name}', '{student_id}')"
+        self.assertEqual(
+            expected_out,
+            output,
+            "The output of your program is not correct.",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
