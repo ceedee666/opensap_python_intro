da045ec4ddac34e69945a62eca6b161707701243
Christian Drumm <christian.drumm@hey.com>
2022-04-03 20:58:48 +0200

    reformatted exercise and moved proposed tests to different file

---
+++ exercises/week_5/assignment/functional_tests.py
@@ -0,0 +1,7 @@
+# Proposed Tests
+
+# - check, if a function `encrypt_letter()` exists
+# - check, if a function `calculate_shifts()` exists
+# - check, if a function `encrypt_text()` exists
+# - check if the firs two functions are nested in the latter one
+# - check inputs
