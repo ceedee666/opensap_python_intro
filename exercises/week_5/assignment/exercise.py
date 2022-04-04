df2ea5eb83519cbb64ab987d0fa3a8d50383136e
Stephan Jacobs <jacobs@fh-aachen.de>
2022-04-04 09:05:50 +0200

    Solved Issues #108 and #109

--- exercises/week_5/assignment/exercise.py
+++ exercises/week_5/assignment/exercise.py
@@ -1,22 +1,34 @@
-def encrypt_letter(let, key):
-    num = ord(let)
-    num -= key
-    if num < 65:
-        num += 26
-    let = chr(num)
-    return let
+def encrypt_letter(letter, shift):
+    abc = "abcdefghijklmnopqrstuvwxyz"
+    ind = abc.index(letter)
+    ind = (ind + shift) % 26
+    secret_letter = abc[ind]
+    return secret_letter
 
-def encrypt_text(text, key):
-    encr_text = ""
-    for char in text:
-        char = char.upper()
-        if char.isalpha():
-            char = encrypt_letter(char, key)
-        encr_text += char
-    return encr_text
+
+def calculate_shifts(letter):
+    abc = "abcdefghijklmnopqrstuvwxyz"
+    ind = abc.index(letter)
+    return ind
+
+
+def encrypt_text(text, keyword):
+    text = text.lower()
+    keyword = keyword.lower()
+
+    encrypted_text = ""
+
+    for i in range(len(text)):
+        key_letter = keyword[i % len(keyword)]
+        shift = calculate_shifts(key_letter)
+        if text[i].isalpha():
+            encrypted_text += encrypt_letter(text[i], shift)
+        else:
+            encrypted_text += text[i]
+    return encrypted_text
 
 
 text = input("Which text should be encrypted: ")
-key = int(input("How many shifts should be taken: "))
+keyword = input("Which keyword should be used? ")
 
-print(encrypt_text(text, key))
+print(encrypt_text(text, keyword))
